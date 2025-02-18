867298a9774ff35177eee6058a7f27ee76a22cf6
Cagkan Caglar <cagkancaglar@hotmail.com>
2025-02-19 00:09:28 +0300

    Add: Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { footerLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
+      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+        <div className="flex flex-col justify-start items-start gap-6">
+          <Image
+            src="/logo.svg"
+            alt="logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
+          <p className="text-base text-gray-700">
+            CarHub 2023 <br /> All rights reserved &copy;
+          </p>
+        </div>
+
+        <div className="footer__links">
+          {footerLinks.map((link, i) => (
+            <div key={i} className="footer__link">
+              <h3 className="font-bold">{link.title}</h3>
+              {link.links.map((item, i) => (
+                <Link key={i} href={item.url} className="text-gray-500" />
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+        <p className="text-base text-gray-700">
+          CarHub 2023 <br /> All rights reserved &copy;
+        </p>
+
+        <div className="footer__copyrights-link">
+          <Link className="text-gray-500" href="/">
+            Privacy Policy
+          </Link>
+          <Link className="text-gray-500" href="/">
+            Term of Use
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
